0c336286d97602ed96f2450aa26089c485de9b0d
heyefu <527380349@qq.com>
2018-03-25 11:17:33 +0800

    增加分类模型和文章模型

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ from django.db import models
 class Category(models.Model):
     """ 分类模型 """
     name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    """ 标签模型 """
+    name = models.CharField(max_length=100)
+
+class Article(models.Model):
+    """ 文章模型 """
+    # 文章名称
+    name = models.CharField(max_length=100)
