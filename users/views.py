06fac3f227a10f35812d0bfc8f031ad6f0dd1370
heyefu <527380349@qq.com>
2018-04-16 17:27:44 +0800

    achieve sign up

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.views import logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 # Create your views here.
+
+
+def logout_view(request):
+    """ 注销用户登录 """
+    logout(request)
+    return HttpResponseRedirect(reverse('blog:index'))
