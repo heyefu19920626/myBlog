7e3663b3b5cb0435212a2053253dff4a33e215ab
heyefu <527380349@qq.com>
2018-04-16 21:38:38 +0800

    register

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.views import logout
+from django.contrib.auth import logout, login, authenticate
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -11,3 +12,19 @@ def logout_view(request):
     """ 注销用户登录 """
     logout(request)
     return HttpResponseRedirect(reverse('blog:index'))
+
+
+def register(request):
+    """ 用户注册 """
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('blog:index'))
+    context = {'form' : form}
+    return render(request, 'users/register.html', context)
